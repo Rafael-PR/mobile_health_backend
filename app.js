0b0e7dfa4cb5b575b64aed13632098afdc4233e6
Rafael <76160982+Rafael-PR@users.noreply.github.com>
2021-04-20 14:38:27 +0200

    fix Heroku

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
 
-const indexRouter = require('./routes/index');
 const clientRouter = require('./routes/clientRouter');
 const bookingRouter = require('./routes/bookingRouter')
 const usersRouter = require('./routes/users');
 const therapistRouter = require('./routes/therapistRouter');
 const authentificationRouter = require('./routes/authentificationRouter');
 
-
 const app = express();
 
-// app.use(cors());
 app.use(cors({
     exposedHeaders: 'x-authorization-token',
   }))
@@ -26,12 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/client', clientRouter);
 app.use('/therapist',therapistRouter);
 app.use('/booking', bookingRouter);
 app.use('/auth',authentificationRouter);
-
-
 
 module.exports = app;
